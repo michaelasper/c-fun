44bd0ca9c915b0d65d39378256d9380f6c43a515
Michael Asper <asper@cs.utexas.edu>
2017-08-28 04:12:44 -0500

    create deck setup

--- Blackjack/src/blackjack.cpp
+++ Blackjack/src/blackjack.cpp
@@ -1,18 +1,75 @@
 #include <iostream>
+#include <string>
 #include "blackjack.h"
 
-namespace blackjack{
+namespace blackjack
+{
+   
     
-    Game::Game(){
 
+
+    void Game::Deck::Card::PrintCard(){
+        if(number > 10 || number == 1){
+            switch(number){
+                case 1:
+                    std::cout << "A" << face;
+                    break;
+                case 11:
+                    std::cout << "J" << face;
+                    break;
+                case 12:
+                    std::cout << "Q" << face;
+                    break;
+                case 13:
+                    std::cout << "K" << face;
+                    break;
+                default:
+                    break; 
+
+            }
+        }else{
+            std::cout << number << face;
+        }
     }
 
-    void Game::Run(){
-        std::cout << "Hello World" << std::endl;
+
+    Game::Deck::Deck()
+    {
+        for (int i = 0; i < 4; ++i)
+        {
+            for (int j = 0; j < 13; ++j)
+            {
+                deckList[(i * 13) + j] = new Deck::Card(j + 1, faceList[i]);
+            }
+        }
     }
 
-    int Game::Main(int argc, const char** argv){
-        Game* game = new Game();
+    void Game::Deck::Shuffle()
+    {
+    }
+
+    void Game::Deck::PrintDeck()
+    {
+        for (int i = 0; i < 52; ++i)
+        {
+            deckList[i]->PrintCard();
+            std::cout << std::endl;
+        }
+    }
+
+    Game::Game()
+    {
+    }
+
+    void Game::Run()
+    {
+        Deck *deck = new Deck();
+        deck->PrintDeck();
+    }
+
+    int Game::Main(int argc, const char **argv)
+    {
+        Game *game = new Game();
         game->Run();
         return 0;
     }
--- Blackjack/src/blackjack.h
+++ Blackjack/src/blackjack.h
@@ -1,11 +1,32 @@
-namespace blackjack{
-    class Game{
-    public:
-        Game();
+namespace blackjack
+{
+    class Game
+    {
+        public:
+            Game();
+            static int Main(int argc, const char **argv);
 
-        static int Main(int argc, const char** argv);
-    private:
-        void Run();
+        private:
+            void Run();
+            class Deck{
+                public:
+                    Deck();
+                    void Shuffle();
+                    void PrintDeck();
+
+                private:
+                    class Card{
+                        private:
+                            int number;
+                            std::string face;
+                        public:
+                            Card();
+                            Card(int number, std::string face): number(number), face(face) {}
+                            void PrintCard();
+                            
+                    };
+                    Card* deckList[52];
+                    std::string faceList[4] = {"♣️","❤","♠️","♦️"};
+            };
     };
-
 }
